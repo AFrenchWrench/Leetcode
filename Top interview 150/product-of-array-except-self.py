350c2dc3a0f1ea1c768b733c8f0b80fda49c240f
AFrenchWrench <AFrenchWrench@gmail.com>
2024-12-11 08:10:09 +0330

    solved product of array except self

---
+++ Top interview 150/product-of-array-except-self.py
@@ -0,0 +1,19 @@
+# my solution for https://leetcode.com/problems/product-of-array-except-self/
+
+
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        answer = [1] * n
+
+        left = 1
+        for i in range(n):
+            answer[i] = left
+            left *= nums[i]
+
+        right = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= right
+            right *= nums[i]
+
+        return answer
