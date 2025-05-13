e0b58210d45828945076dd4f33c527b3ce8ac9f1
AFrenchWrench <AFrenchWrench@gmail.com>
2025-05-13 15:18:59 +0330

    solved concatenation of array

---
+++ array/concatenation of array.py
@@ -0,0 +1,10 @@
+"""
+My solution for https://leetcode.com/problems/concatenation-of-array/
+"""
+
+from typing import List
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums * 2
