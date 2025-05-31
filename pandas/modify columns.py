6dab15cc579f14aba71637becafbf71ec49f440f
AFrenchWrench <AFrenchWrench@gmail.com>
2025-05-31 20:12:03 +0330

    solved modify columns

---
+++ pandas/modify columns.py
@@ -0,0 +1,10 @@
+"""
+My solution for https://leetcode.com/problems/modify-columns/
+"""
+
+import pandas as pd
+
+
+def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["salary"] *= 2
+    return employees
